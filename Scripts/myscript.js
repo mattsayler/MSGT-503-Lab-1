5999f7058922f11f8feeae0ce910c59c268b3f85
Matt Sayler <matt.sayler+code@gmail.com>
2017-10-06 19:09:02 -0700

    Update myscript.js

--- Scripts/myscript.js
+++ Scripts/myscript.js
@@ -8,7 +8,7 @@ document.getElementById("nav01").innerHTML =
 var map = L.map('map',).setView([47.246650, -122.438894],16);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker([47.246650, -122.438894]).addTo(map);
