e921e9e5e5a20c34fb8577f4943b3cff8d01ca20
Matt Sayler <matt.sayler+code@gmail.com>
2017-10-06 18:32:54 -0700

    Update button.js

--- Scripts/button.js
+++ Scripts/button.js
@@ -1,10 +1,21 @@
-function Main(){
-
-  ////browser-dependent css////
-  /*$('div.content').css( {
-    'columns': '290px 5'
-  });*/
-
+function doTheThing(){
   
+  var overlay = $("<div id='overlayDIV'></div>")
+  
+  $("button#nope").click(function(){
+    $("body").after(overlay).css({
+      'background-image':'url("Resources/static.gif")',
+      'position':'fixed',
+      'width':'100%',
+      'height':'100%',
+      'top':'0px',
+      'left':'0px',
+      'z-index':'1000'
+    });
+  });
 
+  $("div#overlayDIV").click(function() {
+    $(this).remove();
+  });
+  
 };
