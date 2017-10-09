c298ed3602f0c4fa1a6b4ebe87ddc34c0616280c
Matt Sayler <matt.sayler+code@gmail.com>
2017-10-08 18:23:24 -0700

    Update myscript.js

--- Scripts/myscript.js
+++ Scripts/myscript.js
@@ -1,3 +1,4 @@
+// Navigation Menu
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li class='left'><a href='index.html'>Home</a></li>" +
@@ -5,10 +6,12 @@ document.getElementById("nav01").innerHTML =
 "<li class='right'><a href='lab1.html'>Labs</a></li>" +
 "</ul>";
 
+// Basic Leaflet map
 var map = L.map('map',).setView([47.246650, -122.438894],16);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Marker for Pinkerton building
 L.marker([47.246650, -122.438894]).addTo(map);
