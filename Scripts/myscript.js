9939ee629b2418bdb33aa3b6050cbe144cc2c058
Matt Sayler <matt.sayler+code@gmail.com>
2017-10-08 17:51:57 -0700

    Update myscript.js

--- Scripts/myscript.js
+++ Scripts/myscript.js
@@ -18,6 +18,6 @@ L.marker([47.246650, -122.438894]).addTo(map);
 //    $(this).remove(); 
 //});
 
-$("div").on("click", "div", function() {
+$(document).on("click", function() {
   console.log("I hear you");
 });
